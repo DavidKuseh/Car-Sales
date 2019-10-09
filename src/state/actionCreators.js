bb8514af1d302bca090f0537596b08e56af132f7
David Kuseh <davegru203@gmail.com>
2019-10-09 13:39:56 +0000

    added state, action types and created reducer function

---
+++ src/state/actionCreators.js
@@ -0,0 +1,9 @@
+import * as types from './actionTypes';
+
+export function addFeature() {
+    return { type: types.ADD_FEATURE };
+}
+
+export function removeFeature() {
+    return { type: types.REMOVE_FEATURE };
+}
